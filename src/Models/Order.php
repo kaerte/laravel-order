f659f26fffdf6ca2152c73fff4b2c1ce7d13cfa1
kaerte <lucas.wski@gmail.com>
2021-06-13 23:59:34 +0200

    Add basket relation

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -4,13 +4,33 @@ declare(strict_types=1);
 
 namespace Ctrlc\Order\Models;
 
+use Ctrlc\Basket\Models\Basket;
 use Ctrlc\Order\Database\Factories\OrderFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'basket_snapshot' => 'array',
+    ];
+
+    public static function createFromBasket(Basket $basket): Order
+    {
+        $order = new self();
+        $order->basket = $basket;
+        $order->save();
+
+        return $order;
+    }
+
+    public function basket(): BelongsTo
+    {
+        return $this->belongsTo(Basket::class);
+    }
 
     protected static function newFactory(): OrderFactory
     {
