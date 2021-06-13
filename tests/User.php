3024a06cb9cf62fee62f2c655cc88689f444c264
kaerte <lucas.wski@gmail.com>
2021-06-13 23:57:24 +0200

    Add Productable trait

--- tests/User.php
+++ tests/User.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Ctrlc\Order\Tests;
 
-use Ctrlc\Address\Traits\HasOrders;
+use Ctrlc\Basket\Traits\HasBasket;
+use Ctrlc\Basket\Traits\Productable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasOrders;
     use HasFactory;
     use Notifiable;
+    use HasBasket;
+    use Productable;
 
     protected $guarded = [];
 
