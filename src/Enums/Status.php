5145d4b6eb7b773c704d5bf643d4ac00fc6d9ecf
kaerte <lucas.wski@gmail.com>
2021-07-19 21:19:50 +0200

    Add Status enum

---
+++ src/Enums/Status.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ctrlc\Order\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self PENDING()
+ * @method static self COMPLETED()
+ * @method static self CANCELLED()
+ * @method static self ARCHIVED()
+ */
+final class Status extends Enum
+{
+}
